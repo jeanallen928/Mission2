e898e4e27f2349042a160c21a5b8402d922ed392
jeanallen928 <123160000+jeanallen928@users.noreply.github.com>
2023-03-26 18:44:32 +0900

    화요일 Mission2 답안 수정
    화요일에 파이썬 기초에서 배운 set(), len(), .index( )활용하여 코드를 줄이고 수포자가 3명을 넘어도 동점자 처리를 할 수 있도록 수정.

--- mission2.py
+++ mission2.py
@@ -8,7 +8,7 @@ def solution(answers):
 
 # 샘플로 내가 만들어본 입력값
 
-answer = [2,1,3,5,1,2,3,4,4,1,3,4,2,5,5,1,3,4,2,5]
+answer = [2,1,2,5,1,2,3,4,4,1,3,4,2,5,5,1,3,4,2,5]
 
 math_test_answers = [
     [1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5],
@@ -36,24 +36,16 @@ for i, a in enumerate(math_test_answers):
 
 # case1) 모두 동점인 경우
 
-if answers_by_testers[0]==answers_by_testers[1]==answers_by_testers[2]:
+if len(set(answers_by_testers)) == 1:
 
     print(f"모두 {total_answer}문제를 맞혔습니다.")
 
 # case2) 모두 다른 점수를 득점한 경우
 
-else:
-    temp = 0
+if len(set(answers_by_testers)) == len(answers_by_testers):
     
-    for i, d in enumerate(answers_by_testers):
-    
-        if temp < answers_by_testers[i]:
-            temp = answers_by_testers[i]
-            max_math = i+1
-    
-        else:
-            pass
     for i, e in enumerate(math_test_answers):
         print(f"수포자 {i + 1}은/는 {answers_by_testers[i]}문제를 맞혔습니다.")
 
-    print(f"따라서 가장 많은 문제를 맞힌 사람은 수포자 {max_math}입니다.")
+    tester_index = answers_by_testers.index(max(answers_by_testers))
+    print(f"따라서 가장 많은 문제를 맞힌 사람은 수포자 {tester_index + 1}입니다.")
