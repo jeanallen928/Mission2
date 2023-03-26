846013bf8d52429e73939228a2a801f670612a48
jeanallen928 <123160000+jeanallen928@users.noreply.github.com>
2023-03-26 22:25:00 +0900

    목요일 동점자가 있을 경우 추가
    파이썬 심화를 듣다가 갑자기 .count()를 이용해서 max()의 개수를 세면 동점자 처리를 할 수 있을 것 같은 생각이 들어서 시도해봤다.
    만점자 빵점자를 고려한 동점자 처리 완료.

--- mission2.py
+++ mission2.py
@@ -11,8 +11,10 @@ def solution(answers):
 # answer = [2,1,2,5,1,2,3,4,4,1,3,4,2,5,5,1,3,4,2,5] #[4,5,6]
 # answer = [2,1,3,5,1,2,3,4,4,1,3,4,2,5,5,1,3,4,2,5] #[5,5,5]
 # answer = [4,4,4,5,4,5,5,1,1,4,4,4,4,5,4,4,5,5,1,4] #[0,0,0]
-answer = [2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1] #[0,4,8]
+# answer = [2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1] #[0,4,8]
 # answer = [1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5] #[20,3,2]
+answer = [2,1,3,5,1,2,2,4,4,1,3,4,2,5,5,1,3,4,2,5] #[6,6,5]
+# answer = [2,1,3,5,1,2,4,4,4,1,3,4,2,5,5,1,3,4,2,5] #[5,5,6]
 
 math_test_answers = [
     [1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5],
@@ -34,6 +36,8 @@ for i, a in enumerate(math_test_answers):
 
     answers_by_testers.append(total_answer)
 
+max_point = max(answers_by_testers)
+
 
 
 # 결과값을 출력
@@ -48,7 +52,7 @@ if len(set(answers_by_testers)) == 1:
 
 # case2) 모두 다른 점수를 득점한 경우
 
-if len(set(answers_by_testers)) == len(answers_by_testers):
+elif len(set(answers_by_testers)) == len(answers_by_testers):
     
     for i, e in enumerate(math_test_answers):
         if answers_by_testers[i]==len(answer):
@@ -60,3 +64,36 @@ if len(set(answers_by_testers)) == len(answers_by_testers):
 
     tester_index = answers_by_testers.index(max(answers_by_testers))
     print(f"따라서 가장 많은 문제를 맞힌 사람은 수포자 {tester_index + 1}입니다.")
+
+# case3) 동점자가 있는 경우
+
+else:
+    if answers_by_testers.count(max_point) > 1:
+        for i, e in enumerate(math_test_answers):
+            if answers_by_testers[i]==len(answer):
+                print(f"수포자 {i + 1}은/는 모든 문제를 맞혔습니다.")
+            elif answers_by_testers[i]==0:
+                print(f"수포자 {i + 1}은/는 모든 문제를 틀렸습니다.")
+            else:
+                print(f"수포자 {i + 1}은/는 {answers_by_testers[i]}문제를 맞혔습니다.")
+        
+        maxs = []
+        
+        for ind, iii in enumerate(answers_by_testers):
+            if iii == max(answers_by_testers):
+                maxs.append(ind)
+        
+        tester_index = answers_by_testers.index(max(answers_by_testers))
+        print(f"따라서 가장 많은 문제를 맞힌 사람은 수포자 {maxs[0] + 1}와/과 수포자 {maxs[1] + 1}입니다.")
+        
+    else:
+        for i, e in enumerate(math_test_answers):
+            if answers_by_testers[i]==len(answer):
+                print(f"수포자 {i + 1}은/는 모든 문제를 맞혔습니다.")
+            elif answers_by_testers[i]==0:
+                print(f"수포자 {i + 1}은/는 모든 문제를 틀렸습니다.")
+            else:
+                print(f"수포자 {i + 1}은/는 {answers_by_testers[i]}문제를 맞혔습니다.")
+
+        tester_index = answers_by_testers.index(max(answers_by_testers))
+        print(f"따라서 가장 많은 문제를 맞힌 사람은 수포자 {tester_index + 1}입니다.")
