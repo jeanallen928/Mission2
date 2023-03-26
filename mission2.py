623c435d56541ae35e0de598cbd75b8c5a5ea578
jeanallen928 <123160000+jeanallen928@users.noreply.github.com>
2023-03-26 21:21:24 +0900

    수요일 수포자들의 찍기 패턴을 이용해 채점하는 것으로 수정 #2
    팀장님 특강을 듣고 나머지를 활용하여 채점하는 방식으로 코드를 수정.

--- mission2.py
+++ mission2.py
@@ -8,27 +8,22 @@ def solution(answers):
 
 # 샘플로 내가 만들어본 입력값
 
-answer = [2,1,2,5,1,2,3,4,4,1,3,4,2,5,5,1,3,4,2,5]
+answers = [2,1,2,5,1,2,3,4,4,1,3,4,2,5,5,1,3,4,2,5]
 
-math_test_answers = [
-    [1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5],
-    [2,1,2,3,2,4,2,5,2,1,2,3,2,4,2,5,2,1,2,3],
-    [3,3,1,1,2,2,4,4,5,5,3,3,1,1,2,2,4,4,5,5]
-]
+answers_of_supos = [[1,2,3,4,5], [2,1,2,3,2,4,2,5], [3,3,1,1,2,2,4,4,5,5]]
 
 
 
-# 진짜 정답과 수포자들의 정답을 비교해서 수포자들의 점수를 계산
+# 정답과 수포자들이 찍은 답을 비교해서 수포자들의 점수를 계산
 
-answers_by_testers = []
+points_of_supos = []
 
-for i, a in enumerate(math_test_answers):
-    total_answer = 0
-    for ii, b in enumerate(math_test_answers[i]):
-        if math_test_answers[i][ii] == answer[ii]:
-            total_answer += 1
-
-    answers_by_testers.append(total_answer)
+for ii in range(len(answers_of_supos)):
+    total_right_answers = 0
+    for i, an in enumerate(answers):
+        if answers_of_supos[ii][i%len(answers_of_supos[ii])] == answers[i]:
+            total_right_answers += 1
+    points_of_supos.append(total_right_answers)
 
 
 
@@ -36,16 +31,16 @@ for i, a in enumerate(math_test_answers):
 
 # case1) 모두 동점인 경우
 
-if len(set(answers_by_testers)) == 1:
+if len(set(points_of_supos)) == 1:
 
-    print(f"모두 {total_answer}문제를 맞혔습니다.")
+    print(f"모두 {total_right_answers}문제를 맞혔습니다.")
 
 # case2) 모두 다른 점수를 득점한 경우
 
-if len(set(answers_by_testers)) == len(answers_by_testers):
+if len(set(points_of_supos)) == len(points_of_supos):
     
-    for i, e in enumerate(math_test_answers):
-        print(f"수포자 {i + 1}은/는 {answers_by_testers[i]}문제를 맞혔습니다.")
+    for i, e in enumerate(answers_of_supos):
+        print(f"수포자 {i + 1}은/는 {points_of_supos[i]}문제를 맞혔습니다.")
 
-    tester_index = answers_by_testers.index(max(answers_by_testers))
+    tester_index = points_of_supos.index(max(points_of_supos))
     print(f"따라서 가장 많은 문제를 맞힌 사람은 수포자 {tester_index + 1}입니다.")
